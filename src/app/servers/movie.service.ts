bd84904d40b79aed605ab89d8c8f56601182fbc9
deepak belamana <saideepak627@gmail.com>
2023-06-19 23:37:55 +0530

    added local api url to point to local api

--- src/app/servers/movie.service.ts
+++ src/app/servers/movie.service.ts
@@ -8,11 +8,11 @@ export class MovieService {
 
   //api deployed on railway app
    baseProductionApiUrl : String ="https://broken-whistle-production-818f.up.railway.app/"
-
+   baseLocalApiUrl:String = "http://localhost:8080/"
   constructor(private http : HttpClient) {
   }
 
   getAllMoviesList(){
-    return this.http.get(this.baseProductionApiUrl+"moviesList")
+    return this.http.get(this.baseLocalApiUrl+"moviesList")
   }
 }
